8799dd1c3e4d3210721e3f970c9450b22ca5fb9c
0xnabil <110378255+0xnabil@users.noreply.github.com>
2024-03-04 17:09:28 +0100

    Add black formatter (#2)
    * black formatter in Github Actions
    
    * code formatted
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,18 +1,19 @@
 import pytest
 import sys
-import os 
+import os
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)) + "/trending-pools")
 
 from helpers import parse_token_id
 
+
 @pytest.mark.parametrize(
-        "id, chain_and_address",
-        [
-            ("polygon_pos_0xCrazyShitcoin0", ("polygon_pos", "0xCrazyShitcoin0")),
-            ("eth_0xCrazyShitcoin1", ("eth", "0xCrazyShitcoin1")),
-            ("eth-goerli_0xCrazyShitcoin2", ("eth-goerli", "0xCrazyShitcoin2")),
-        ]
+    "id, chain_and_address",
+    [
+        ("polygon_pos_0xCrazyShitcoin0", ("polygon_pos", "0xCrazyShitcoin0")),
+        ("eth_0xCrazyShitcoin1", ("eth", "0xCrazyShitcoin1")),
+        ("eth-goerli_0xCrazyShitcoin2", ("eth-goerli", "0xCrazyShitcoin2")),
+    ],
 )
 def test_parse_token_id(id, chain_and_address):
     assert parse_token_id(id) == chain_and_address
